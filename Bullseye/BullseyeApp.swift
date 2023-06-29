8321b29456815a85c4f31adadd483994e0349204
Aditya Rohman <aditya.rohman@koltiva.com>
2023-06-29 18:34:55 +0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by aditya.rohman on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
